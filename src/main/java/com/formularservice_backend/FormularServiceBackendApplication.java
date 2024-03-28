34dc9bf4987f1ae23e632659c7a93b8f8ba34215
Rida Labkakla <147744110+rlabka@users.noreply.github.com>
2024-03-28 16:21:01 +0100

    Initial Commit

---
+++ src/main/java/com/formularservice_backend/FormularServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayono.formularservice_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormularServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mayono/formularservice_backend/FormularServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayono.formularservice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
